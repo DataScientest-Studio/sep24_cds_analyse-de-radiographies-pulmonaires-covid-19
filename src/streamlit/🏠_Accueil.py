8814233216aa4ece04450075147a62acf6104940
Alban Lemaître <48556361+albanlemaitre@users.noreply.github.com>
2025-07-01 11:38:41 +0200

    Add files via upload

---
+++ src/streamlit/🏠_Accueil.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Accueil - Détection pathologies pulmonaires",
+    page_icon="🏠",
+    layout="wide",
+    initial_sidebar_state="expanded" 
+)
+
+col1, col2 = st.columns([1, 20])
+with col1:
+    st.image("../images/Normal-2.png", width=170)
+with col2:
+    st.title("Détection de pathologies pulmonaires à partir de radiographies thoraciques")
+
+st.write("_Projet basé sur le rapport d'analyse de radiographies pour la détection de la COVID-19 et autres pathologies._")
+
+
+st.write("### Introduction")
+
+
+st.write("""
+Le diagnostic médical par imagerie radiologique est un élément clé de la médecine moderne. Toutefois, l’interprétation manuelle des radiographies thoraciques est souvent complexe, sujette à erreurs humaines et dépend fortement de l’expertise du radiologue.
+
+Ce projet, réalisé dans le cadre de la formation **Data Scientist - Septembre 2024** chez Data Scientest, vise à développer un pipeline automatique de classification des radiographies pulmonaires. L’objectif principal est la détection précise de la COVID-19, de la pneumonie virale et d'autres pathologies pulmonaires.
+
+La méthodologie comprend une exploration et un pré-traitement approfondis des données, suivi de la comparaison entre modèles classiques de Machine Learning et architectures avancées de Deep Learning, incluant des techniques de transfer learning et des modèles hybrides CNN-ViT.
+
+Les performances des modèles sont évaluées via plusieurs métriques clés : précision, sensibilité (rappel), spécificité, F1-score pondéré et matrice de confusion.
+""")
