38a5b3b5dea1569e839689724d7c7b4ee0bf3979
Alban Lemaître <48556361+albanlemaitre@users.noreply.github.com>
2025-07-03 22:23:43 +0200

    Update 1_📊_Analyse_des_données.py

--- src/streamlit/pages/1_📊_Analyse_des_données.py
+++ src/streamlit/pages/1_📊_Analyse_des_données.py
@@ -72,7 +72,9 @@ selection = st.segmented_control("", options, selection_mode="single"
 )
 if selection == "PCA" :
     st.write("#### Analyse en Composantes Principales (PCA)")
-    input_filename = '../data/pca_3d_data.csv'
+    script_dir = os.path.dirname(os.path.abspath(__file__))    
+    project_root = os.path.dirname(script_dir)    
+    input_filename = os.path.join(project_root, 'data', 'pca_3d_data.csv')    
     df_plot = pd.read_csv(input_filename)  
     fig = px.scatter_3d(
         df_plot,
@@ -94,7 +96,9 @@ if selection == "PCA" :
 
 if selection == "PCA" :
     st.write("#### Uniform Manifold Approximation and Projection (UMAP)")
-    input_filename = '../data/umap_3d_data.csv'
+    script_dir = os.path.dirname(os.path.abspath(__file__))    
+    project_root = os.path.dirname(script_dir)    
+    input_filename = os.path.join(project_root, 'data', 'umap_3d_data.csv')    
     df_plot = pd.read_csv(input_filename)  
     fig = px.scatter_3d(
         df_plot,
@@ -116,7 +120,9 @@ if selection == "PCA" :
     
 elif selection == "AE" :
     st.write("#### Auto-Encoder (AE)")
-    input_filename = '../data/ae_3d_data.csv'
+    script_dir = os.path.dirname(os.path.abspath(__file__))    
+    project_root = os.path.dirname(script_dir)    
+    input_filename = os.path.join(project_root, 'data', 'ae_3d_data.csv')    
     df_plot = pd.read_csv(input_filename)  
     fig = px.scatter_3d(
         df_plot,
@@ -139,7 +145,9 @@ elif selection == "AE" :
     interactive_image("src/images/Autoencoder.png", "exemple")
 if selection == "NMF" :
     st.write("#### NMF : Non-negative Matrix Factorization")
-    input_filename = '../data/nmf_3d_data.csv'
+    script_dir = os.path.dirname(os.path.abspath(__file__))    
+    project_root = os.path.dirname(script_dir)    
+    input_filename = os.path.join(project_root, 'data', 'nmf_3d_data.csv')    
     df_plot = pd.read_csv(input_filename)  
     fig = px.scatter_3d(
         df_plot,
