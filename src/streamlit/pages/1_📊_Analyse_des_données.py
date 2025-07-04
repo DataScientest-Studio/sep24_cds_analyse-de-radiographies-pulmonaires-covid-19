27307b1ebcf95ba08ac113e51b13d60a5a633509
Alban Lemaître <48556361+albanlemaitre@users.noreply.github.com>
2025-07-04 16:15:23 +0200

    Update 1_📊_Analyse_des_données.py

--- src/streamlit/pages/1_📊_Analyse_des_données.py
+++ src/streamlit/pages/1_📊_Analyse_des_données.py
@@ -178,6 +178,7 @@ outliers_selection = st.segmented_control("", outliers_options, selection_mode="
 if outliers_selection == "Statistique" :
     st.write("*Statistique*")
     st.write("Cette méthode fondamentale transforme chaque image en un vecteur de trois caractéristiques numériques : la moyenne (luminosité globale), l'écart-type (contraste) et l'entropie (complexité/quantité d'information). Un score d'anomalie est ensuite calculé pour chaque image en mesurant sa distance par rapport au centre de la distribution de toutes les images. Une image très sombre, très blanche ou très simple (peu de détails) obtiendra un score élevé.")
+    script_dir = os.path.dirname(os.path.abspath(__file__))
     project_root = os.path.dirname(script_dir)    
     input_filename = os.path.join(project_root, 'data', 'outliers_statistique.csv')    
     df_plot = pd.read_csv(input_filename)  
