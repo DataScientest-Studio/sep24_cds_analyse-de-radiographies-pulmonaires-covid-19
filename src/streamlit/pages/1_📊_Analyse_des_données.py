0ab2d96e7948009ca042165cd367a3838a553009
Alban Lemaître <48556361+albanlemaitre@users.noreply.github.com>
2025-07-04 21:18:33 +0200

    Update 1_📊_Analyse_des_données.py

--- src/streamlit/pages/1_📊_Analyse_des_données.py
+++ src/streamlit/pages/1_📊_Analyse_des_données.py
@@ -371,12 +371,12 @@ else:
         st.write("*Image Originale*")
         if original_image:
             file_name = os.path.basename(st.session_state.current_image_path)
-            st.image(original_image, caption=f"Fichier : {file_name}", width=400) 
+            st.image(original_image, caption=f"Fichier : {file_name}", width=200) 
 
     with image_col2:
         st.write("*Image Transformée*")
         if st.session_state.transformed_image is not None:
-            st.image(st.session_state.transformed_image, caption="Transformation + Normalisation", width=400)
+            st.image(st.session_state.transformed_image, caption="Transformation + Normalisation", width=200)
         else:
              st.info("Cliquez sur 'Appliquer une transformation' pour générer une version augmentée.")
 
