b5550a0368e93176af038790139dabb15c3b2f48
Alban Lemaître <48556361+albanlemaitre@users.noreply.github.com>
2025-07-04 22:46:11 +0200

    Update 1_📊_Analyse_des_données.py

--- src/streamlit/pages/1_📊_Analyse_des_données.py
+++ src/streamlit/pages/1_📊_Analyse_des_données.py
@@ -90,7 +90,8 @@ fig.update_layout(
 )
 st.plotly_chart(fig, use_container_width=True)
 
-interactive_image("src/images/Variance.png", "exemple")
+# Remplacé par graphique interactif plotly en violin plot
+# interactive_image("src/images/Variance.png", "exemple")
 
 st.subheader("Détection d'anomalies")
 st.write("""
@@ -103,110 +104,6 @@ Ci-dessous une visualisation de la répartition de l’intensité en fonction de
 """)
 interactive_image("src/images/Intensite-ecart.png", "exemple")
 
-st.subheader("Réductions de dimensions")
-
-options = ["PCA", "AE", "NMF", "UMAP"]
-selection = st.segmented_control("", options, selection_mode="single"
-)
-
-palette = {
-    'Normal': 'green',
-    'COVID': 'red',
-    'Lung_Opacity': 'orange',
-    'Viral Pneumonia': 'blue'}
-
-
-if selection == "PCA" :
-    st.write("#### Analyse en Composantes Principales (PCA)")
-    st.write("PCA trouve de nouveaux axes (appelés composantes principales) qui maximisent la variance (la dispersion) des données. Le premier axe capture le plus de variance possible, le deuxième en capture le plus possible parmi ce qu'il reste, et ainsi de suite. C'est une méthode purement mathématique et linéaire.")
-    script_dir = os.path.dirname(os.path.abspath(__file__))    
-    project_root = os.path.dirname(script_dir)    
-    input_filename = os.path.join(project_root, 'data', 'pca_3d_data.csv')    
-    df_plot = pd.read_csv(input_filename)  
-    fig = px.scatter_3d(
-        df_plot,
-        x='PCA 1',
-        y='PCA 2',
-        z='PCA 3',
-        color='label', 
-        title="Visualisation 3D PCA des radiographies pulmonaires",
-        color_discrete_map=palette,
-    )
-    fig.update_traces(marker=dict(size=3, opacity=0.8))
-    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
-    fig.update_traces(hoverinfo='none', hovertemplate=None)
-    st.plotly_chart(fig, use_container_width=True)
-
-if selection == "UMAP" :
-    st.write("#### Uniform Manifold Approximation and Projection (UMAP)")
-    st.write("UMAP est une technique d'apprentissage de variétés (manifold learning). Elle suppose que les données, même si elles sont dans un grand espace, vivent en réalité sur une surface de plus faible dimension (la variété). UMAP essaie de modéliser cette surface et de la déplier dans un espace plus petit tout en préservant au mieux la structure topologique des données (qui est voisin de qui, localement et globalement).")
-    script_dir = os.path.dirname(os.path.abspath(__file__))    
-    project_root = os.path.dirname(script_dir)    
-    input_filename = os.path.join(project_root, 'data', 'umap_3d_data.csv')    
-    df_plot = pd.read_csv(input_filename)  
-    fig = px.scatter_3d(
-        df_plot,
-        x='UMAP 1',
-        y='UMAP 2',
-        z='UMAP 3',
-        color='label', 
-        title="Visualisation 3D UMAP des radiographies pulmonaires",
-        color_discrete_map=palette,
-    )
-    fig.update_traces(marker=dict(size=3, opacity=0.8))
-    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
-    fig.update_traces(hoverinfo='none', hovertemplate=None)
-    st.plotly_chart(fig, use_container_width=True)
-
-    
-elif selection == "AE" :
-    st.write("#### Auto-Encoder (AE)")
-    st.write("C'est un type de réseau de neurones qui apprend à compresser les données (partie encodeur) en une représentation de faible dimension (le goulot d'étranglement ou bottleneck), puis à les décompresser (partie décodeur) pour reconstruire l'entrée originale. En forçant le réseau à recréer les données à partir d'une version compressée, il apprend les caractéristiques les plus importantes.")
-    script_dir = os.path.dirname(os.path.abspath(__file__))    
-    project_root = os.path.dirname(script_dir)    
-    input_filename = os.path.join(project_root, 'data', 'ae_3d_data.csv')    
-    df_plot = pd.read_csv(input_filename)  
-    fig = px.scatter_3d(
-        df_plot,
-        x='AE 1',
-        y='AE 2',
-        z='AE 3',
-        color='label', 
-        title="Visualisation 3D AE des radiographies pulmonaires",
-        color_discrete_map=palette,
-    )
-    fig.update_traces(marker=dict(size=3, opacity=0.8))
-    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
-    fig.update_traces(hoverinfo='none', hovertemplate=None)
-    st.plotly_chart(fig, use_container_width=True)
-
-    
-if selection == "NMF" :
-    st.write("#### Non-negative Matrix Factorization (NMF)")
-    st.write("La NMF décompose une grande matrice de données (par exemple, des images ou des documents) en deux matrices plus petites. La contrainte essentielle est que toutes les valeurs dans les trois matrices doivent être non-négatives. Cela force la décomposition à être additive.")
-    script_dir = os.path.dirname(os.path.abspath(__file__))    
-    project_root = os.path.dirname(script_dir)    
-    input_filename = os.path.join(project_root, 'data', 'nmf_3d_data.csv')    
-    df_plot = pd.read_csv(input_filename)  
-    fig = px.scatter_3d(
-        df_plot,
-        x='NMF 1',
-        y='NMF 2',
-        z='NMF 3',
-        color='label', 
-        title="Visualisation 3D NMF des radiographies pulmonaires",
-        color_discrete_map=palette,
-    )
-    fig.update_traces(marker=dict(size=3, opacity=0.8))
-    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
-    fig.update_traces(hoverinfo='none', hovertemplate=None)
-    st.plotly_chart(fig, use_container_width=True)
-
-
-
-st.subheader("Prétraitement")
-
-st.write("Plusieurs techniques de prétraitement ont été appliquées durant le projet. Ces étapes se sont avérées très utiles pour améliorer la performance des modèles de machine learning.")
 
 st.write("#### Elimination des anomalies")
 
@@ -339,6 +236,113 @@ elif selection == "Auto-encoder":
                     st.image(Image.open(image_path), use_container_width=True)
                 except FileNotFoundError:
                     st.markdown(f"_(Image #{rank} non trouvée)_")   
+
+
+
+st.subheader("Réductions de dimensions")
+
+options = ["PCA", "AE", "NMF", "UMAP"]
+selection = st.segmented_control("", options, selection_mode="single"
+)
+
+palette = {
+    'Normal': 'green',
+    'COVID': 'red',
+    'Lung_Opacity': 'orange',
+    'Viral Pneumonia': 'blue'}
+
+
+if selection == "PCA" :
+    st.write("#### Analyse en Composantes Principales (PCA)")
+    st.write("PCA trouve de nouveaux axes (appelés composantes principales) qui maximisent la variance (la dispersion) des données. Le premier axe capture le plus de variance possible, le deuxième en capture le plus possible parmi ce qu'il reste, et ainsi de suite. C'est une méthode purement mathématique et linéaire.")
+    script_dir = os.path.dirname(os.path.abspath(__file__))    
+    project_root = os.path.dirname(script_dir)    
+    input_filename = os.path.join(project_root, 'data', 'pca_3d_data.csv')    
+    df_plot = pd.read_csv(input_filename)  
+    fig = px.scatter_3d(
+        df_plot,
+        x='PCA 1',
+        y='PCA 2',
+        z='PCA 3',
+        color='label', 
+        title="Visualisation 3D PCA des radiographies pulmonaires",
+        color_discrete_map=palette,
+    )
+    fig.update_traces(marker=dict(size=3, opacity=0.8))
+    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
+    fig.update_traces(hoverinfo='none', hovertemplate=None)
+    st.plotly_chart(fig, use_container_width=True)
+
+if selection == "UMAP" :
+    st.write("#### Uniform Manifold Approximation and Projection (UMAP)")
+    st.write("UMAP est une technique d'apprentissage de variétés (manifold learning). Elle suppose que les données, même si elles sont dans un grand espace, vivent en réalité sur une surface de plus faible dimension (la variété). UMAP essaie de modéliser cette surface et de la déplier dans un espace plus petit tout en préservant au mieux la structure topologique des données (qui est voisin de qui, localement et globalement).")
+    script_dir = os.path.dirname(os.path.abspath(__file__))    
+    project_root = os.path.dirname(script_dir)    
+    input_filename = os.path.join(project_root, 'data', 'umap_3d_data.csv')    
+    df_plot = pd.read_csv(input_filename)  
+    fig = px.scatter_3d(
+        df_plot,
+        x='UMAP 1',
+        y='UMAP 2',
+        z='UMAP 3',
+        color='label', 
+        title="Visualisation 3D UMAP des radiographies pulmonaires",
+        color_discrete_map=palette,
+    )
+    fig.update_traces(marker=dict(size=3, opacity=0.8))
+    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
+    fig.update_traces(hoverinfo='none', hovertemplate=None)
+    st.plotly_chart(fig, use_container_width=True)
+
+    
+elif selection == "AE" :
+    st.write("#### Auto-Encoder (AE)")
+    st.write("C'est un type de réseau de neurones qui apprend à compresser les données (partie encodeur) en une représentation de faible dimension (le goulot d'étranglement ou bottleneck), puis à les décompresser (partie décodeur) pour reconstruire l'entrée originale. En forçant le réseau à recréer les données à partir d'une version compressée, il apprend les caractéristiques les plus importantes.")
+    script_dir = os.path.dirname(os.path.abspath(__file__))    
+    project_root = os.path.dirname(script_dir)    
+    input_filename = os.path.join(project_root, 'data', 'ae_3d_data.csv')    
+    df_plot = pd.read_csv(input_filename)  
+    fig = px.scatter_3d(
+        df_plot,
+        x='AE 1',
+        y='AE 2',
+        z='AE 3',
+        color='label', 
+        title="Visualisation 3D AE des radiographies pulmonaires",
+        color_discrete_map=palette,
+    )
+    fig.update_traces(marker=dict(size=3, opacity=0.8))
+    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
+    fig.update_traces(hoverinfo='none', hovertemplate=None)
+    st.plotly_chart(fig, use_container_width=True)
+
+    
+if selection == "NMF" :
+    st.write("#### Non-negative Matrix Factorization (NMF)")
+    st.write("La NMF décompose une grande matrice de données (par exemple, des images ou des documents) en deux matrices plus petites. La contrainte essentielle est que toutes les valeurs dans les trois matrices doivent être non-négatives. Cela force la décomposition à être additive.")
+    script_dir = os.path.dirname(os.path.abspath(__file__))    
+    project_root = os.path.dirname(script_dir)    
+    input_filename = os.path.join(project_root, 'data', 'nmf_3d_data.csv')    
+    df_plot = pd.read_csv(input_filename)  
+    fig = px.scatter_3d(
+        df_plot,
+        x='NMF 1',
+        y='NMF 2',
+        z='NMF 3',
+        color='label', 
+        title="Visualisation 3D NMF des radiographies pulmonaires",
+        color_discrete_map=palette,
+    )
+    fig.update_traces(marker=dict(size=3, opacity=0.8))
+    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
+    fig.update_traces(hoverinfo='none', hovertemplate=None)
+    st.plotly_chart(fig, use_container_width=True)
+
+
+
+st.subheader("Prétraitement")
+
+st.write("Plusieurs techniques de prétraitement ont été appliquées durant le projet. Ces étapes se sont avérées très utiles pour améliorer la performance des modèles de machine learning.")
 
 st.write("#### Redimensionnement et normalisation")
 
