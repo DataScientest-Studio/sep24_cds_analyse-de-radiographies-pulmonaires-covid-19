45bb2b75bce98b06b8e539d39d5970e1a12ad5e9
Alban Lemaître <48556361+albanlemaitre@users.noreply.github.com>
2025-07-07 22:14:30 +0200

    Update 1_📊_Analyse_des_données.py

--- src/streamlit/pages/1_📊_Analyse_des_données.py
+++ src/streamlit/pages/1_📊_Analyse_des_données.py
@@ -441,8 +441,7 @@ options = ["PCA", "UMAP", "AE", "NMF"]
 selection = st.segmented_control("", 
                                  options, 
                                  selection_mode="single",
-                                 default="PCA"
-                                )
+                                 default="PCA")
 
 palette = {
     'Normal': 'green',
@@ -481,66 +480,83 @@ if selection == "PCA" :
 if selection == "UMAP" :
     st.write("#### Uniform Manifold Approximation and Projection (UMAP)")
     st.write("UMAP est une technique d'apprentissage de variétés (manifold learning). Elle suppose que les données, même si elles sont dans un grand espace, vivent en réalité sur une surface de plus faible dimension (la variété). UMAP essaie de modéliser cette surface et de la déplier dans un espace plus petit tout en préservant au mieux la structure topologique des données (qui est voisin de qui, localement et globalement).")
-    script_dir = os.path.dirname(os.path.abspath(__file__))    
-    project_root = os.path.dirname(script_dir)    
-    input_filename = os.path.join(project_root, 'data', 'umap_3d_data.csv')    
-    df_plot = pd.read_csv(input_filename)  
-    fig = px.scatter_3d(
-        df_plot,
-        x='UMAP 1',
-        y='UMAP 2',
-        z='UMAP 3',
-        color='label', 
-        color_discrete_map=palette
-    )
-    fig.update_traces(marker=dict(size=3, opacity=0.8))
-    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
-    fig.update_traces(hoverinfo='none', hovertemplate=None)
-    st.plotly_chart(fig, use_container_width=True)
+    
+    col_gauche, col_droite = st.columns([1, 1])
 
+    with col_gauche:
+        script_dir = os.path.dirname(os.path.abspath(__file__))    
+        project_root = os.path.dirname(script_dir)    
+        input_filename = os.path.join(project_root, 'data', 'umap_3d_data.csv')    
+        df_plot = pd.read_csv(input_filename)  
+        fig = px.scatter_3d(
+            df_plot,
+            x='UMAP 1',
+            y='UMAP 2',
+            z='UMAP 3',
+            color='label', 
+            color_discrete_map=palette
+        )
+        fig.update_traces(marker=dict(size=3, opacity=0.8))
+        fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
+        fig.update_traces(hoverinfo='none', hovertemplate=None)
+        st.plotly_chart(fig, use_container_width=True)
+   
+with col_droite:
+        st.image("src/streamlit/dimensionality_reduction/umap_visualization_with_thumbnails.png", use_container_width=True)
     
 elif selection == "AE" :
     st.write("#### Auto-Encoder (AE)")
     st.write("C'est un type de réseau de neurones qui apprend à compresser les données (partie encodeur) en une représentation de faible dimension (le goulot d'étranglement ou bottleneck), puis à les décompresser (partie décodeur) pour reconstruire l'entrée originale. En forçant le réseau à recréer les données à partir d'une version compressée, il apprend les caractéristiques les plus importantes.")
-    script_dir = os.path.dirname(os.path.abspath(__file__))    
-    project_root = os.path.dirname(script_dir)    
-    input_filename = os.path.join(project_root, 'data', 'ae_3d_data.csv')    
-    df_plot = pd.read_csv(input_filename)  
-    fig = px.scatter_3d(
-        df_plot,
-        x='AE 1',
-        y='AE 2',
-        z='AE 3',
-        color='label', 
-        color_discrete_map=palette
-    )
-    fig.update_traces(marker=dict(size=3, opacity=0.8))
-    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
-    fig.update_traces(hoverinfo='none', hovertemplate=None)
-    st.plotly_chart(fig, use_container_width=True)
+    
+    col_gauche, col_droite = st.columns([1, 1])
 
+    with col_gauche:
+        script_dir = os.path.dirname(os.path.abspath(__file__))    
+        project_root = os.path.dirname(script_dir)    
+        input_filename = os.path.join(project_root, 'data', 'ae_3d_data.csv')    
+        df_plot = pd.read_csv(input_filename)  
+        fig = px.scatter_3d(
+            df_plot,
+            x='AE 1',
+            y='AE 2',
+            z='AE 3',
+            color='label', 
+            color_discrete_map=palette
+        )
+        fig.update_traces(marker=dict(size=3, opacity=0.8))
+        fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
+        fig.update_traces(hoverinfo='none', hovertemplate=None)
+        st.plotly_chart(fig, use_container_width=True)
+    
+    with col_droite:
+        st.image("src/streamlit/dimensionality_reduction/autoencoder_visualization_with_thumbnails.png", use_container_width=True)
     
 if selection == "NMF" :
     st.write("#### Non-negative Matrix Factorization (NMF)")
     st.write("La NMF décompose une grande matrice de données (par exemple, des images ou des documents) en deux matrices plus petites. La contrainte essentielle est que toutes les valeurs dans les trois matrices doivent être non-négatives. Cela force la décomposition à être additive.")
-    script_dir = os.path.dirname(os.path.abspath(__file__))    
-    project_root = os.path.dirname(script_dir)    
-    input_filename = os.path.join(project_root, 'data', 'nmf_3d_data.csv')    
-    df_plot = pd.read_csv(input_filename)  
-    fig = px.scatter_3d(
-        df_plot,
-        x='NMF 1',
-        y='NMF 2',
-        z='NMF 3',
-        color='label', 
-        color_discrete_map=palette
-    )
-    fig.update_traces(marker=dict(size=3, opacity=0.8))
-    fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
-    fig.update_traces(hoverinfo='none', hovertemplate=None)
-    st.plotly_chart(fig, use_container_width=True)
+    
+    col_gauche, col_droite = st.columns([1, 1])
 
+    with col_gauche:
+        script_dir = os.path.dirname(os.path.abspath(__file__))    
+        project_root = os.path.dirname(script_dir)    
+        input_filename = os.path.join(project_root, 'data', 'nmf_3d_data.csv')    
+        df_plot = pd.read_csv(input_filename)  
+        fig = px.scatter_3d(
+            df_plot,
+            x='NMF 1',
+            y='NMF 2',
+            z='NMF 3',
+            color='label', 
+            color_discrete_map=palette
+        )
+        fig.update_traces(marker=dict(size=3, opacity=0.8))
+        fig.update_layout(legend_title_text='Classe',margin=dict(l=0, r=0, b=0, t=0))
+        fig.update_traces(hoverinfo='none', hovertemplate=None)
+        st.plotly_chart(fig, use_container_width=True)
 
+    with col_droite:
+        st.image("src/streamlit/dimensionality_reduction/nmf_visualization_with_thumbnails.png", use_container_width=True)
 
 st.subheader("Prétraitement")
 
