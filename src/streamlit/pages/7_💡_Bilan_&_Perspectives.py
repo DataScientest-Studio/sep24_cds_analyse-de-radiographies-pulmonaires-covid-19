437d41a6b8e6507343ca98585c06150d6128d309
Alban Lemaître <48556361+albanlemaitre@users.noreply.github.com>
2025-07-07 20:20:03 +0200

    Update 7_💡_Bilan_&_Perspectives.py

--- src/streamlit/pages/7_💡_Bilan_&_Perspectives.py
+++ src/streamlit/pages/7_💡_Bilan_&_Perspectives.py
@@ -42,16 +42,13 @@ st.write("""
 with st.expander("🔬 DenseNet-121 + Vision Transformer (ViT)"):
     st.write("""
     DenseNet-121 est un réseau CNN structuré en quatre blocs denses. Chaque couche reçoit les sorties de toutes les couches précédentes, améliorant ainsi la réutilisation des caractéristiques extraites et limitant la disparition du gradient.  
-    ViT (Vision Transformer), quant à lui, segmente l’image en **patches** pour appliquer une **self-attention globale**.  
-    Cette approche hybride exploite **les forces combinées** :  
-    - locales → CNN  
-    - globales → Transformer  
+    ViT (Vision Transformer), quant à lui, segmente l’image en patches pour appliquer une self-attention globale.  
+    Cette approche hybride exploite les forces combinées :  
+    - du **CNN** → qui capture les dépendances **locales** 
+    - du **Vision Transformer** → qui capture les dépendances **globales** 
 
-    **F1-score pondéré : 0.99, Précision pondérée : 0.99**  
-    Optimisé via **Grid Search** sur :  
-    - le learning rate  
-    - le taux de dropout  
-    - la taille des batchs
+    **f1-score pondéré : 0.99, précision pondérée : 0.99**  
+    Optimisé via Grid Search sur le learning rate, le taux de dropout, la taille des batchs
     """)
 st.write("""
   - Intégration de modules d’attention : pour améliorer la focalisation du réseau sur les régions critiques.
@@ -59,16 +56,14 @@ st.write("""
 with st.expander("🔬 VGG16 + SE (Squeeze-and-Excitation)"):
     st.write("""
     VGG16 est un CNN profond à 16 couches, introduit en 2014.  
-    Les blocs SE (Squeeze-and-Excitation) appliquent une **attention par canal**, permettant au réseau de **recalibrer dynamiquement** les canaux d’activation.  
-    Cette attention est insérée après chaque bloc convolutif, avant le max-pooling.
-
-    **F1-score pondéré : 0.9864, Précision pondérée : 0.9864**
+    Les blocs **SE (Squeeze-and-Excitation)** appliquent une **attention par canal**, permettant au réseau de **recalibrer dynamiquement** les canaux d’activation.  
+    Cette attention est **insérée après chaque bloc convolutif**, avant le max-pooling.
+    **f1-score pondéré : 0.9864, précision pondérée : 0.9864**
     """)
-
-
-st.markdown("---")
-st.subheader("📉 Conclusion")
-st.write("""
-Contrairement aux attentes, ces modèles avancés **ne surpassent pas significativement** les modèles CNN fine-tunés comme EfficientNet ou InceptionV3.  
-En revanche, leur coût en calcul est **nettement plus élevé**, notamment à cause des modules d’attention.
+st.write(""" 
+  → Contrairement aux attentes, ces modèles avancés ne surpassent pas significativement les modèles CNN fine-tunés comme EfficientNet ou InceptionV3.  
+En revanche, leur coût en calcul est plus élevé, notamment à cause des modules d’attention.
 """)
+
+st.subheader("Conclusion")
+
