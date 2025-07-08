449fd5dfe850bfc413bcebd9c0242630131c20ca
Alban Lemaître <48556361+albanlemaitre@users.noreply.github.com>
2025-07-08 11:19:36 +0200

    Update 4_🧠_Modèles_DL.py

--- src/streamlit/pages/4_🧠_Modèles_DL.py
+++ src/streamlit/pages/4_🧠_Modèles_DL.py
@@ -189,7 +189,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)    
     col1, col2 = st.columns(2)
     with col1:
-        st.image(image, caption="Image fournie par l'utilisateur", use_column_width=True)
+        st.image(image, caption="Image fournie par l'utilisateur", use_container_width=True)
     with col2:
         with st.spinner("Prédiction en cours..."):
             tracker = EmissionsTracker(project_name="streamlit_inference", log_level="error")
